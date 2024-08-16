d1d405f9b82a6b228fc2fe30e50720c5131d8f82
Chaos-19 <kalgetachew375@gmail.com>
2024-08-16 13:09:55 +0300

    update template to locate static files

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from .config import Config
-from app.routes.main import main_bp
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+
+
 
 
 db = SQLAlchemy()
@@ -12,13 +14,19 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     
-    db.init_app(app)
+    db.init_app(app=app)
     migrate.init_app(app, db)
+    
+    if not os.path.exists(Config.UPLOAD_FOLDER):
+        os.makedirs(Config.UPLOAD_FOLDER)
 
     # Register blueprints
     from .routes import register_blue_prints
     register_blue_prints(app)
-
-
+    
+    with app.app_context():
+        from .models.model import User
+        db.create_all()
+        
     
     return app
